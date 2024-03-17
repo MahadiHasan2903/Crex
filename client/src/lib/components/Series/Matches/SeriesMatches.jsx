d8abeb0b1a22ffbab8d13a8dde3c1432a9b19259
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2024-03-17 23:00:08 +0600

    completed squads of series page

--- client/src/lib/components/Series/Matches/SeriesMatches.jsx
+++ client/src/lib/components/Series/Matches/SeriesMatches.jsx
@@ -1,7 +1,123 @@
 import React from "react";
+import Image from "next/image";
+
+export const matchDays = [
+  {
+    day: "Wednesday, February 2024",
+    matches: [
+      {
+        type: "1st T20, PSL 2024",
+        team1: { name: "PSZ", score: "199/5", overs: "20.00", logo: "/BD.png" },
+        result: { outcome: "ISU Won", margin: "by 21 runs" },
+        team2: {
+          name: "ISU",
+          score: "220/10",
+          overs: "20.00",
+          logo: "/BD.png",
+        },
+      },
+      {
+        type: "2nd T20, PSL 2024",
+        team1: { name: "PSZ", score: "225/5", overs: "20.00", logo: "/BD.png" },
+        result: { outcome: "PSZ Won", margin: "by 8 wickets" },
+        team2: { name: "MS", score: "220/10", overs: "19.3", logo: "/BD.png" },
+      },
+    ],
+  },
+  {
+    day: "Thursday, February 2024",
+    matches: [
+      {
+        type: "3rd T20, PSL 2024",
+        team1: { name: "KK", score: "180/7", overs: "20.00", logo: "/KK.png" },
+        result: { outcome: "KK Won", margin: "by 10 runs" },
+        team2: { name: "LQ", score: "170/8", overs: "20.00", logo: "/LQ.png" },
+      },
+      {
+        type: "4th T20, PSL 2024",
+        team1: { name: "QG", score: "205/5", overs: "20.00", logo: "/QG.png" },
+        result: { outcome: "Match Tied", margin: "" },
+        team2: { name: "MS", score: "205/5", overs: "20.00", logo: "/MS.png" },
+      },
+    ],
+  },
+  {
+    day: "Friday, February 2024",
+    matches: [
+      {
+        type: "5th T20, PSL 2024",
+        team1: { name: "LQ", score: "190/6", overs: "20.00", logo: "/LQ.png" },
+        result: { outcome: "LQ Won", margin: "by 5 wickets" },
+        team2: { name: "KK", score: "185/7", overs: "20.00", logo: "/KK.png" },
+      },
+      {
+        type: "6th T20, PSL 2024",
+        team1: { name: "QG", score: "215/4", overs: "20.00", logo: "/QG.png" },
+        result: { outcome: "QG Won", margin: "by 15 runs" },
+        team2: { name: "PSZ", score: "200/9", overs: "20.00", logo: "/BD.png" },
+      },
+    ],
+  },
+];
 
 const SeriesMatches = () => {
-  return <div>SeriesMatches</div>;
+  return (
+    <div className="flex justify-center text-[#4A4A4A] dark:text-[#CFD1D3]">
+      <div className="w-[75%]">
+        <div className="w-[70%] dark:text-[#9FA3A7] text-black pr-10 border-r border-black gap-x-5 dark:border-white border-opacity-5 dark:border-opacity-5">
+          <p className="text-[24px] font-bold pt-10">PSL 2024 Matches</p>
+          {matchDays.map((day) => (
+            <div key={day.day}>
+              <p className="text-[14px] mb-5 mt-10">{day.day}</p>
+              {day.matches.map((match) => (
+                <div
+                  key={match.type}
+                  className="dark:text-[#9FA3A7] text-[#4A4A4A] mt-10"
+                >
+                  <p className="text-[11px] text-[#4A4A4A] dark:text-[#9FA3A7] ">
+                    {match.type}
+                  </p>
+                  <div className="px-2 pb-1 border-b border-black dark:border-white border-opacity-5 dark:border-opacity-5">
+                    <div className="py-4 flex cursor-pointer mb-1 dark:hover:bg-[#16232F] rounded-md hover:bg-[#FBFBFB] items-center justify-between mt-2 dark:text-[#9FA3A7] text-[#4A4A4A]">
+                      <div className="flex items-center gap-x-2">
+                        <Image src="/BD.png" width={50} height={50} />
+                        <div>
+                          <p className="text-[16px]">{match.team1.name}</p>
+                          <div className="flex items-center gap-x-2">
+                            <p className="font-medium text-[16px] text-[#4A4A4A] dark:text-[#CFD1D3]">
+                              {match.team1.score}
+                            </p>
+                            <p className="text-[14px]">{match.team1.overs}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-center justify-center">
+                        <p className="text-[#BB7627] text-[16px]">
+                          {match.result.outcome}
+                        </p>
+                        <p className="text-[12px]">{match.result.margin}</p>
+                      </div>
+                      <div className="flex items-center gap-x-2">
+                        <div className="flex flex-col items-end">
+                          <p className="text-[16px]">{match.team2.name}</p>
+                          <div className="flex items-center gap-x-2">
+                            <p className="font-medium text-[16px] text-[#4A4A4A] dark:text-[#CFD1D3]">
+                              {match.team2.score}
+                            </p>
+                            <p className="text-[14px]">{match.team2.overs}</p>
+                          </div>
+                        </div>
+                        <Image src="/BD.png" width={50} height={50} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default SeriesMatches;
--- client/src/lib/components/Series/Squads/SeriesSquads.jsx
+++ client/src/lib/components/Series/Squads/SeriesSquads.jsx
@@ -1,7 +1,243 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+
+export const teams = [
+  {
+    name: "Peshawar Zalmi",
+    playersCount: 27,
+    imageUrl: "/peshawar_zalmi_logo.png",
+  },
+  {
+    name: "Karachi Kings",
+    playersCount: 25,
+    imageUrl: "/karachi_kings_logo.png",
+  },
+  {
+    name: "Islamabad United",
+    playersCount: 23,
+    imageUrl: "/islamabad_united_logo.png",
+  },
+  {
+    name: "Lahore Qalandars",
+    playersCount: 26,
+    imageUrl: "/lahore_qalandars_logo.png",
+  },
+  {
+    name: "Quetta Gladiators",
+    playersCount: 24,
+    imageUrl: "/quetta_gladiators_logo.png",
+  },
+  {
+    name: "Multan Sultans",
+    playersCount: 22,
+    imageUrl: "/multan_sultans_logo.png",
+  },
+  {
+    name: "Karachi Kings",
+    playersCount: 25,
+    imageUrl: "/karachi_kings_logo.png",
+  },
+];
+
+export const players = [
+  { name: "Babar Azam", type: "Batsman" },
+  { name: "Shaheen Afridi", type: "Bowler" },
+  { name: "Shadab Khan", type: "All-Rounder" },
+  { name: "Kamran Akmal", type: "Batsman" },
+  { name: "Wahab Riaz", type: "Bowler" },
+  { name: "Darren Sammy", type: "All-Rounder" },
+  { name: "Haider Ali", type: "Batsman" },
+  { name: "Mohammad Amir", type: "Bowler" },
+  { name: "Imran Tahir", type: "Bowler" },
+  { name: "David Miller", type: "Batsman" },
+  { name: "Chris Gayle", type: "Batsman" },
+  { name: "Kieron Pollard", type: "All-Rounder" },
+  { name: "Shoaib Malik", type: "All-Rounder" },
+  { name: "Tom Banton", type: "Batsman" },
+  { name: "Carlos Brathwaite", type: "All-Rounder" },
+  { name: "Umar Akmal", type: "Batsman" },
+  { name: "Liam Livingstone", type: "All-Rounder" },
+  { name: "Mujeeb Ur Rahman", type: "Bowler" },
+  { name: "Rahat Ali", type: "Bowler" },
+  { name: "Mohammad Irfan", type: "Bowler" },
+  { name: "Liam Dawson", type: "All-Rounder" },
+];
 
 const SeriesSquads = () => {
-  return <div>SeriesSquads</div>;
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleTeamClick = (index) => {
+    console.log("Active Index:", index);
+    setActiveIndex(index);
+  };
+
+  const selectedTeam = activeIndex !== null ? teams[activeIndex] : teams[0];
+
+  return (
+    <div className="flex justify-center text-[#4A4A4A] dark:text-[#CFD1D3]">
+      <div className="w-[75%] pl-5">
+        <p className="text-[24px] font-bold pt-10">PSL 2024 Squads</p>
+        <div className="flex items-start mt-4 gap-x-5">
+          <div className="w-1/3 my-5 overflow-y-scroll">
+            {teams.map((team, index) => (
+              <div
+                key={index}
+                className={`flex w-full items-center py-4 px-5 gap-x-5 cursor-pointer  ${
+                  activeIndex === index
+                    ? "bg-[#E7EFF6] dark:bg-[#10202B] border-r-[2px] border-[#1860A6]"
+                    : "bg-transparent border-b border-black dark:border-white border-opacity-5 dark:border-opacity-5"
+                }`}
+                onClick={() => handleTeamClick(index)}
+              >
+                <Image src="/team.png" width={48} height={48} alt="team" />
+                <div>
+                  <p className="text-[16px] ">{team.name}</p>
+                  <p className="text-[14px] text-[#4A4A4A] dark:text-[#9FA3A7]">
+                    {team.playersCount} players
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="text-[#4A4A4A] dark:text-[#9FA3A7] w-full mx-5 mt-5">
+            <div className="flex items-center py-2 border-b border-black dark:border-white border-opacity-5 dark:border-opacity-5">
+              <Image src="/team.png" width={72} height={72} alt="team" />
+              <div>
+                <p className="text-[18px] font-medium">{selectedTeam.name}</p>
+                <p className="text-[14px] opacity-70 ">
+                  {selectedTeam.playersCount} Players
+                </p>
+              </div>
+            </div>
+            <div className="w-full my-10">
+              <p className="text-[16px] font-medium">
+                Bat (
+                {players.filter((player) => player.type === "Batsman").length})
+              </p>
+              <div className="grid w-full grid-cols-3 my-4 ">
+                {players.map((player, index) => {
+                  if (player.type === "Batsman") {
+                    return (
+                      <div
+                        key={index}
+                        className="flex items-center pb-2 my-2 border-b border-black gap-x-5 dark:border-white border-opacity-5 dark:border-opacity-5"
+                      >
+                        <div>
+                          <Image
+                            src="/player.png"
+                            width={77}
+                            height={75}
+                            className="-mb-5"
+                          />
+                          <Image
+                            src="/jersey.png"
+                            width={77}
+                            height={30}
+                            className="z-[9994]"
+                          />
+                        </div>
+                        <div className="mt-5">
+                          <p className="text-[16px] dark:text-[#EAAE54] text-[#9E6105]">
+                            {player.name}
+                          </p>
+                          <p className="text-[15px]">Batsman</p>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+            </div>
+            <div className="w-full my-10">
+              <p className="text-[16px] font-medium">
+                Bowl (
+                {players.filter((player) => player.type === "Bowler").length})
+              </p>
+              <div className="grid w-full grid-cols-3 my-4 ">
+                {players.map((player, index) => {
+                  if (player.type === "Bowler") {
+                    return (
+                      <div
+                        key={index}
+                        className="flex items-center pb-2 my-2 border-b border-black gap-x-5 dark:border-white border-opacity-5 dark:border-opacity-5"
+                      >
+                        <div>
+                          <Image
+                            src="/player.png"
+                            width={77}
+                            height={75}
+                            className="-mb-5"
+                          />
+                          <Image
+                            src="/jersey.png"
+                            width={77}
+                            height={30}
+                            className="z-[9994]"
+                          />
+                        </div>
+                        <div className="mt-5">
+                          <p className="text-[16px] dark:text-[#EAAE54] text-[#9E6105]">
+                            {player.name}
+                          </p>
+                          <p className="text-[15px]">Batsman</p>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+            </div>
+            <div className="w-full my-10">
+              <p className="text-[16px] font-medium">
+                AR (
+                {
+                  players.filter((player) => player.type === "All-Rounder")
+                    .length
+                }
+                )
+              </p>
+              <div className="grid w-full grid-cols-3 my-4 ">
+                {players.map((player, index) => {
+                  if (player.type === "All-Rounder") {
+                    return (
+                      <div
+                        key={index}
+                        className="flex items-center pb-2 my-2 border-b border-black gap-x-5 dark:border-white border-opacity-5 dark:border-opacity-5"
+                      >
+                        <div>
+                          <Image
+                            src="/player.png"
+                            width={77}
+                            height={75}
+                            className="-mb-5"
+                          />
+                          <Image
+                            src="/jersey.png"
+                            width={77}
+                            height={30}
+                            className="z-[9994]"
+                          />
+                        </div>
+                        <div className="mt-5">
+                          <p className="text-[16px] dark:text-[#EAAE54] text-[#9E6105]">
+                            {player.name}
+                          </p>
+                          <p className="text-[15px]">Batsman</p>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SeriesSquads;
